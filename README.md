386b13c4e3210ee5fed50e624d85bcccda056d04
totojack <luca.iovio@gmail.com>
2023-01-06 15:45:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# CDK TypeScript Workshop
+
+- The workshop: [CDK TypeScript Workshop](https://cdkworkshop.com/20-typescript.html)
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
+
+## VSCode Dev Container
+
+You can use the dev container in VSCode if you don't want to install all the required tools.
---
+++ lib/aws_cdk_workshop-stack.ts
@@ -0,0 +1,34 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
+
+export class AwsCdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    //lambda hello
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    //lambda counter
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    //api gw
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    });
+
+  }
+}
